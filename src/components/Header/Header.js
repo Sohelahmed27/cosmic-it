ea7c733018cdf01ef45e6aadb43715b625146c2b
Sohelahmed27 <sohel.gebcu@gmail.com>
2023-03-31 06:18:56 +0600

    header-added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <>
+     <Navbar bg="light" variant="light">
+        <Container className="d-flex justify-content-between ">
+          <Navbar.Brand href="#home">COSMIC IT-HUB</Navbar.Brand>
+          <Nav className="">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Header;
